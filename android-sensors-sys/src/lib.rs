b797ea8415a648fcec5671c606b1079582e521eb
Daniel Santana <danielgsantana@gmail.com>
2024-03-14 22:20:20 +0000

    Remove cfg

--- android-sensors-sys/src/lib.rs
+++ android-sensors-sys/src/lib.rs
@@ -1,7 +1,6 @@
 mod logging;
 mod sensors;
 
-#[cfg(target_os = "android")]
 pub mod ffi {
     pub mod sensors {
         pub use crate::sensors::*;
--- android-sensors/src/lib.rs
+++ android-sensors/src/lib.rs
@@ -1,10 +1,5 @@
-#[cfg(target_os = "android")]
 pub mod looper;
-#[cfg(target_os = "android")]
 pub mod manager;
-#[cfg(target_os = "android")]
 pub mod sensor;
-#[cfg(target_os = "android")]
 pub mod sensor_queue;
-#[cfg(target_os = "android")]
 mod utils;
